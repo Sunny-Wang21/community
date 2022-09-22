24a35554f0c7dcb29b41b27dcbb951ea92835363
wangziyao <2075000180@qq.com>
2022-09-22 20:39:29 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -0,0 +1,48 @@
+package com.nowcoder.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/alpha")
+public class AlphaController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello Spring Boot.";
+    }
+
+    //利用thymeleaf模板返回html
+    @RequestMapping(path = "/teacher",method = RequestMethod.GET)
+    public ModelAndView getTeacher(){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name","张三");
+        mav.addObject("age",20);
+        mav.setViewName("/demo/view");
+        return mav;
+    }
+    @RequestMapping(path = "/school",method = RequestMethod.GET)
+    public String getSchool(Model model){
+        model.addAttribute("name","北京大学");
+        model.addAttribute("age",80);
+        return "/demo/view";
+
+    }
+    @RequestMapping(path = "/emp",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> getEmp(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("姓名","张三");
+        map.put("年龄",19);
+        map.put("薪水",8000);
+        return map;
+
+    }
+}
